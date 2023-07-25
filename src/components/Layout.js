d52acf03dca86ac2c86e3f50fc76eee6a8d599a3
ShevchukA <antoshka.vbg@mail.ru>
2023-07-25 08:00:15 +0300

    feat: add routing menu

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,30 @@
 import classes from "./Layout.module.css";
+import { Menu } from "antd";
+import { EnvironmentOutlined } from "@ant-design/icons";
 
 function Layout() {
+  const items = [
+    { label: "Маршрут 1", key: "1", icon: <EnvironmentOutlined /> },
+    { label: "Маршрут 2", key: "2", icon: <EnvironmentOutlined /> },
+    { label: "Маршрут 3", key: "3", icon: <EnvironmentOutlined /> },
+  ];
+
+  function selectionHandler(e) {
+    console.log("click ", e);
+  }
+
   return (
     <div className={classes.container}>
-      <nav></nav>
+      <nav>
+        <p>Выберите Ваш маршрут:</p>
+        <Menu
+          items={items}
+          defaultSelectedKeys={["1"]}
+          mode="inline"
+          theme="dark"
+          onClick={selectionHandler}
+        />
+      </nav>
       <main></main>
     </div>
   );
