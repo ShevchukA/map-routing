e1cd6318284ce539f01d06eb70370f120c704490
ShevchukA <antoshka.vbg@mail.ru>
2023-08-06 17:13:52 +0300

    fix: update root saga logic

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,10 +1,13 @@
-import { takeEvery, takeLatest } from "redux-saga/effects";
+import { takeEvery, takeLatest, all } from "redux-saga/effects";
 import handleGetRoutes from "./handleGetRoutes";
 import handleSelectRoute from "./handleSelectRoute";
 
 import { GET_ROUTES, SELECT_ROUTE } from "../constants";
 
 export default function* rootSaga() {
-  yield takeEvery(GET_ROUTES, handleGetRoutes); // следим за вызовом экшена типа GET_ROUTES
-  yield takeLatest(SELECT_ROUTE, handleSelectRoute); // следим за вызовом экшена типа SELECT_ROUTE
+  yield all([
+    // saga-watchers
+    yield takeEvery(GET_ROUTES, handleGetRoutes), // следим за вызовом экшена типа GET_ROUTES
+    yield takeLatest(SELECT_ROUTE, handleSelectRoute), // следим за вызовом экшена типа SELECT_ROUTE
+  ]);
 }
